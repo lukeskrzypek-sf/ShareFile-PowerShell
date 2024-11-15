6ff961ba7cf57bc555de7636c120495b02adffde
Luke Skrzypek <luke.skrzypek@cloud.com>
2024-11-14 20:45:14 -0500

    feature: Replace old Browser control with WebView2
    New requirements:
    Net Framework 4.8 installed
    WebView2 runtime installed

--- ShareFileSnapIn/Browser/OAuthAuthenticationForm.cs
+++ ShareFileSnapIn/Browser/OAuthAuthenticationForm.cs
@@ -1,15 +1,10 @@
-﻿using Newtonsoft.Json;
-using ShareFile.Api.Models;
+﻿using Microsoft.Web.WebView2.Core;
+using ShareFile.Api.Powershell.Extensions;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ShareFile.Api.Powershell.Browser
@@ -17,57 +12,105 @@ namespace ShareFile.Api.Powershell.Browser
     public partial class OAuthAuthenticationForm : Form
     {
         public delegate bool UrlEventCallback(Uri uri);
-        private Dictionary<string, UrlEventCallback> _urlEventHandlers = new Dictionary<string,UrlEventCallback>();
+        private readonly Dictionary<string, UrlEventCallback> _urlEventHandlers = new Dictionary<string,UrlEventCallback>();
 
-        public delegate bool DocumentEventCallback(Uri uri, Stream document);
-        private List<DocumentEventCallback> _documentEventHandlers = new List<DocumentEventCallback>();
+        private readonly Dictionary<ulong, string> _navigationIdToUrlMapping = new Dictionary<ulong, string>();
+
+        private readonly CoreWebView2EnvironmentOptions webView2EnvOptions = new CoreWebView2EnvironmentOptions(
+                                    additionalBrowserArguments: "--disable-features=msSmartScreenProtection",
+                                    allowSingleSignOnUsingOSPrimaryAccount: true);
 
         public OAuthAuthenticationForm()
         {
             InitializeComponent();
-            browser.AllowNavigation = true;
-            browser.ScriptErrorsSuppressed = false;
-            browser.ScrollBarsEnabled = true;
-            browser.TabIndex = 1;
+
+            InitializeWebView2();
         }
 
         public void AddUrlEventHandler(string uri, UrlEventCallback handler)
         {
             _urlEventHandlers.Add(uri, handler);
         }
-
-        public void AddDocumentEventHander(DocumentEventCallback handler)
-        {
-            _documentEventHandlers.Add(handler);
-        }
-
-        public void SetDocument(Uri uri, StreamReader sr)
-        {
-            string s = sr.ReadToEnd();
-            browser.Navigate("about:blank");
-            browser.Document.OpenNew(false);
-            browser.Document.Write(s);
-            browser.Refresh();
-            browser.Url = uri;
-        }
-
         public void Navigate(Uri uri)
         {
-            browser.Navigate(uri);
+            webViewBrowser.CoreWebView2.Navigate(uri.ToString());
             ShowDialog();
         }
 
-        private void browser_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        private void InitializeWebView2()
         {
-            foreach (var uri in _urlEventHandlers.Keys)
+            // Select the cache directory 
+            string localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string cacheFolder = Path.Combine(localAppData, "ShareFile", "ShareFile PowerShell SDK");
+
+            var env = TaskHelper.RunSynchronously(CoreWebView2Environment.CreateAsync(null, cacheFolder, webView2EnvOptions));
+
+            TaskHelper.RunSynchronously(webViewBrowser.EnsureCoreWebView2Async(env));
+
+            webViewBrowser.CoreWebView2.ProcessFailed += CoreWebView2_ProcessFailed;
+            webViewBrowser.CoreWebView2.Settings.AreDefaultContextMenusEnabled = false;
+            webViewBrowser.CoreWebView2.Settings.AreDevToolsEnabled = false;
+            webViewBrowser.CoreWebView2.Settings.AreBrowserAcceleratorKeysEnabled = false;
+            webViewBrowser.CoreWebView2.ClientCertificateRequested += CoreWebView2_ClientCertificateRequested;
+
+            webViewBrowser.NavigationStarting += WebViewBrowser_NavigationStarting;
+            webViewBrowser.NavigationCompleted += WebViewBrowser_NavigationCompleted;
+        }
+
+        private void CoreWebView2_ClientCertificateRequested(object sender, CoreWebView2ClientCertificateRequestedEventArgs e)
+        {
+            var certificateList = e.MutuallyTrustedCertificates;
+
+            if (certificateList.Count() == 1)
             {
-                if (e.Url.ToString().StartsWith(uri))
+                // If there is only one client certificate, then just use that one instead of prompting the user to click on it.
+                e.SelectedCertificate = certificateList[0];
+                e.Handled = true;
+            }
+        }
+
+        private void CoreWebView2_ProcessFailed(object sender, CoreWebView2ProcessFailedEventArgs e)
+        {
+            LogProcessFailedEventArgs(e);
+
+            // TODO: Better handling/logging of WebView2 process failures
+        }
+
+        private void LogProcessFailedEventArgs(CoreWebView2ProcessFailedEventArgs e)
+        {
+            Console.Error.WriteLine($"WebView2 process failed: Reason: {e.Reason} Exit code: {e.ExitCode} ProcessFailedKind: {e.ProcessFailedKind} Process description: {e.ProcessDescription}");
+            if ((e.FrameInfosForFailedProcess?.Any()).GetValueOrDefault())
+            {
+                var sb = new StringBuilder("FrameInfos");
+                foreach (var frameInfo in e.FrameInfosForFailedProcess)
                 {
-                    if (_urlEventHandlers[uri].Invoke(e.Url))
+                    sb.AppendFormat("\tName: {0}, Source: {1} ({2})", frameInfo.Name, frameInfo.Source, frameInfo.ToString());
+                    sb.AppendLine();
+                }
+                Console.Error.WriteLine(sb.ToString());
+            }
+        }
+
+        private void WebViewBrowser_NavigationStarting(object sender, CoreWebView2NavigationStartingEventArgs e)
+        {
+            _navigationIdToUrlMapping[e.NavigationId] = e.Uri;
+        }
+
+        private void WebViewBrowser_NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e)
+        {
+            if (_navigationIdToUrlMapping.TryGetValue(e.NavigationId, out var url))
+            {
+                foreach (var handler in _urlEventHandlers)
+                {
+                    if (url.ToString().StartsWith(handler.Key))
                     {
-                        this.Close();
+                        if (handler.Value.Invoke(new Uri(url)))
+                        {
+                            this.Close();
+                        }
                     }
                 }
+                _navigationIdToUrlMapping.Remove(e.NavigationId);
             }
         }
     }
